9cc5e20cd8e941b59db0c28cd97a0aded58ff469
Thomas Ravache <thomasravache31@gmail.com>
2024-06-15 22:42:28 -0300

    feat: 04. Models - Transaction

---
+++ Dima/Dima.Core/Models/Transaction.cs
@@ -0,0 +1,15 @@
+namespace Dima.Core.Models;
+
+public class Transaction
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime? PaidOrReceivedAt { get; set; }
+    public int Type { get; set; }
+    public decimal Amount { get; set; }
+    public long CategoryId { get; set; }
+    public Category Category { get; set; } = null!;
+
+    public string UserId { get; set; } = string.Empty;
+}
