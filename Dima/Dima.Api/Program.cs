3c3df799af74f7f4c9ee47ecb06c8ee03a3dd09a
Thomas Ravache <thomasravache31@gmail.com>
2024-06-16 16:16:14 -0300

    feat: 14. Swagger

--- Dima/Dima.Api/Program.cs
+++ Dima/Dima.Api/Program.cs
@@ -1,5 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x =>
+{
+x.CustomSchemaIds(n => n.FullName); // Pega o nome das classes para documentação, evitando conflitos entre nomes iguais de classes
+});
+
 var app = builder.Build();
+
+app.UseSwagger(); // informa que iremos utilizar o swagger
+app.UseSwaggerUI(); // informa que iremos utilizar a UI do swagger
 
 app.MapGet("/v1/categories", () => "Hello World!");
 app.MapGet("/v2/categories", () => "Hello World!");
