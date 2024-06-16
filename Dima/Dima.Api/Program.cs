44c3f067e6e5efb4ae39c962b8bc8217af147aa3
Thomas Ravache <thomasravache31@gmail.com>
2024-06-15 23:23:04 -0300

    feat: 08. URLs e Endpoints

--- Dima/Dima.Api/Program.cs
+++ Dima/Dima.Api/Program.cs
@@ -2,5 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapPost("/", () => "Hello World!");
+app.MapPut("/", () => "Hello World!");
+app.MapDelete("/", () => "Hello World!");
 
 app.Run();
