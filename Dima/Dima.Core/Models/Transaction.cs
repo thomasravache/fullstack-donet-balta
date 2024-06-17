ebd1bbc65dc0ac69f39dbab7817547941de6ad0e
Thomas Ravache <thomasravache31@gmail.com>
2024-06-17 08:10:14 -0300

    feat: 20. Mapeamento de Movimentacao

--- Dima/Dima.Core/Models/Transaction.cs
+++ Dima/Dima.Core/Models/Transaction.cs
@@ -12,6 +12,5 @@ public class Transaction
     public decimal Amount { get; set; }
     public long CategoryId { get; set; }
     public Category Category { get; set; } = null!;
-
     public string UserId { get; set; } = string.Empty;
 }
