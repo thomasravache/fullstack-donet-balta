b1d36d67dbc2421b602bc200cd3b90be39e1f02a
Thomas Ravache <thomasravache31@gmail.com>
2024-06-17 07:33:37 -0300

    feat: 18. DbContext e DbSet

--- Dima/Dima.Api/Data/AppDbContext.cs
+++ Dima/Dima.Api/Data/AppDbContext.cs
@@ -1,6 +1,16 @@
+using Dima.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace Dima.Api.Data;
 
-public class AppDbContext
+public class AppDbContext : DbContext
 {
+    public DbSet<Category> Categories { get; set; } = null!;
+    public DbSet<Transaction> Transactions { get; set; } = null!;
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // configuracoes do banco de dados
+        modelBuilder.ApplyConfiguration(); // será implementado
+    }
 }
