ff6ff75a8a2ffc9d29008e7a5010501e20deba81
Thomas Ravache <thomasravache31@gmail.com>
2024-06-17 07:55:29 -0300

    feat: 19. Mapeamento de Categoria

--- Dima/Dima.Core/Models/Category.cs
+++ Dima/Dima.Core/Models/Category.cs
@@ -4,6 +4,6 @@ public class Category
 {
     public long Id { get; set; }
     public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+    public string? Description { get; set; } = string.Empty;
     public string UserId { get; set; } = string.Empty;
 }
