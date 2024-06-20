a61850f3eef5713ad56fb79d0f939084d01ce6d1
Thomas Ravache <thomasravache31@gmail.com>
2024-06-20 07:46:30 -0300

    feat: 46. Padronização dos Endpoints

---
+++ Dima/Dima.Api/Endpoints/Categories/CreateCategoryEndpoint.cs
@@ -0,0 +1,11 @@
+using Dima.Api.Common.Api;
+
+namespace Dima.Api.Endpoints.Categories;
+
+public class CreateCategoryEndpoint : IEndpoint
+{
+    public void Map(IEndpointRouteBuilder app)
+    {
+        throw new NotImplementedException();
+    }
+}
