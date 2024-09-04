8ed4d845c1071d9aaf617e6992a5b86953f07bc0
travache <thomasravache31@gmail.com>
2024-09-04 00:15:34 -0300

    feat: 60. IdentityDbContext

--- Dima/Dima.Api/Data/AppDbContext.cs
+++ Dima/Dima.Api/Data/AppDbContext.cs
@@ -1,11 +1,22 @@
 using System.Reflection;
+using Dima.Api.Models;
 using Dima.Core.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Dima.Api.Data;
 
-public class AppDbContext : IdentityDbContext
+public class AppDbContext : IdentityDbContext<
+    User, // AspNetUsers
+    IdentityRole<long>, // AspNetRoles (AspnetUserRoles)
+    long, // Claim, Login, RoleClaim, Tokens
+    IdentityUserClaim<long>,
+    IdentityUserRole<long>,
+    IdentityUserLogin<long>,
+    IdentityRoleClaim<long>,
+    IdentityUserToken<long>
+>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options) {  }
