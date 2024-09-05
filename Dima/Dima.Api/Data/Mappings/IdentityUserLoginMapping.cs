a24f103bcb7df20f99ddb1f393f604902a8cc83c
travache <thomasravache31@gmail.com>
2024-09-04 22:02:15 -0300

    feat: 62. Mapeando tabelas do Identity

---
+++ Dima/Dima.Api/Data/Mappings/IdentityUserLoginMapping.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dima.Api.Data.Mappings;
+
+public class IdentityUserLoginMapping : IEntityTypeConfiguration<IdentityUserLogin<long>>
+{
+    public void Configure(EntityTypeBuilder<IdentityUserLogin<long>> builder)
+    {
+        builder.ToTable("IdentityUserLogin");
+        builder.HasKey(l => new { l.LoginProvider, l.ProviderKey });
+        builder.Property(l => l.LoginProvider).HasMaxLength(128);
+        builder.Property(l => l.ProviderKey).HasMaxLength(128);
+        builder.Property(l => l.ProviderDisplayName).HasMaxLength(255);
+    }
+}
