6b95ad6ae86a44e92b393ec4a2bd8f28f927811b
travache <thomasravache31@gmail.com>
2024-09-05 23:01:34 -0300

    feat: 65. Logout

--- Dima/Dima.Api/Program.cs
+++ Dima/Dima.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using System.Text.Json.Serialization;
 using Asp.Versioning;
 using Dima.Api;
@@ -84,8 +85,66 @@ app.UseExceptionHandler();
 app.MapGet("/", () => new { message = "OK" });
 app.MapEndpoints();
 
-app.MapGroup("v1/identity")
+app.MapGroup("api/v1/identity")
     .WithTags("Identity")
-    .MapIdentityApi<User>();
+    .MapIdentityApi<User>(); // endpoints padrão do Identity
+
+app.MapGroup("api/v1/identity")
+    .WithTags("Identity")
+    .MapPost("/logout", async (SignInManager<User> signInManager, UserManager<User> _a, RoleManager<IdentityRole<long>> _b) =>
+    {
+        // limpar cookies de navegação e o signout será feito
+        await signInManager.SignOutAsync();
+
+        return Results.Ok();
+    })
+    .RequireAuthorization();
+
+app.MapGroup("api/v1/identity")
+    .WithTags("Identity")
+    .MapGet("/roles", async (
+        SignInManager<User> _,
+        UserManager<User> userManager,
+        RoleManager<IdentityRole<long>> roleManager,
+        ClaimsPrincipal signedUser
+        ) =>
+    {
+        // limpar cookies de navegação e o signout será feito
+        var user = await userManager.FindByEmailAsync(""); // buscar pelo banco o usuario pelo email
+        var userRoles = user?.Roles; // roles do usuário pegos pelo banco de dados
+
+        // ou usando o roleManager
+        var roles = await roleManager.Roles.ToListAsync(); // todas as roles existentes
+
+        // obter usuário logado com o ClaimsPrincipal
+        
+        if (signedUser.Identity is null || !signedUser.Identity.IsAuthenticated)
+            return Results.Unauthorized();
+
+        // aqui pega os roles pelos cookies sem precisar pegar no banco
+            // vantagem: não precisa bater no banco
+            // desvantagem: se o tempo de expiração do cookie for muito grande ele vai retornar que o usuário tenha uma permissão que já tenha sido retirada até o cookie expirar
+        var identity = (ClaimsIdentity)signedUser.Identity;
+        var signedUserRoles = identity.FindAll(identity.RoleClaimType)
+            .Select(x => new
+            {
+                x.Issuer, // quem emitiu
+                x.OriginalIssuer,
+                x.Type, // tipo do claim
+                x.Value,
+                x.ValueType
+            });
+
+        return Results.Ok(new
+        {
+            roles,
+            signedUser = new
+            {
+                roles = signedUserRoles
+            }
+        });
+    })
+    .RequireAuthorization();
+
 
 app.Run();
