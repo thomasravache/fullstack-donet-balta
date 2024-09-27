ea17dc5d9c6cf52d7d5c6be143dc70c5ee32cea0
travache <thomasravache31@gmail.com>
2024-09-27 06:30:07 -0300

    feat: 90. Definindo os Servicos

---
+++ Dima/Dima.Core/Requests/Account/RegisterRequest.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Dima.Core.Requests.Account;
+
+public class RegisterRequest
+{
+    [Required(ErrorMessage = "E-mail")]
+    [EmailAddress(ErrorMessage = "E-mail Inválido")]
+    public string Email { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Senha Inválida")]
+    public string Password { get; set; } = string.Empty;
+}
