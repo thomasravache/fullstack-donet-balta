435a66b2e32610679ad2fc2db15d06bc778bdaaf
travache <thomasravache31@gmail.com>
2024-10-01 08:34:39 -0300

    feat: 97. Claims

--- Dima/Dima.Core/Models/Account/User.cs
+++ Dima/Dima.Core/Models/Account/User.cs
@@ -4,5 +4,6 @@ public class User
 {
     // v1/identity/manage/info - retem infos do usuário
     public string Email { get; set; } = string.Empty;
+    public bool IsEmailConfirmed { get; set; }
     public Dictionary<string, string> Type { get; set; } = [];
 }
--- Dima/Dima.Web/Security/CookieAuthenticationStateProvider.cs
+++ Dima/Dima.Web/Security/CookieAuthenticationStateProvider.cs
@@ -1,3 +1,6 @@
+using System.Net.Http.Json;
+using System.Security.Claims;
+using Dima.Core.Models.Account;
 using Microsoft.AspNetCore.Components.Authorization;
 
 namespace Dima.Web.Security;
@@ -19,6 +22,29 @@ public class CookieAuthenticationStateProvider(
     public override Task<AuthenticationState> GetAuthenticationStateAsync()
     {
         throw new NotImplementedException();
+    }
+
+    private async Task<User?> GetUser()
+    {
+        try
+        {
+            return await _client.GetFromJsonAsync<User?>("v1/identity/manage/info");
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    private async Task<List<Claim> GetClaims(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.Name, user.Email),
+            new(ClaimTypes.Email, user.Email) // Name é o identificador do usuário que estamos passando o email no caso
+        };
+
+        return claims;
     }
 
     public void NotifyAuthenticationStateChanged()
