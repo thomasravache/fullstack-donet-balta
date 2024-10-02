4167e4bee7926df0eb77fc76f44e03f09a70d162
travache <thomasravache31@gmail.com>
2024-10-02 07:47:48 -0300

    feat: 99. Get Authentication State

--- Dima/Dima.Web/Security/CookieAuthenticationStateProvider.cs
+++ Dima/Dima.Web/Security/CookieAuthenticationStateProvider.cs
@@ -19,9 +19,25 @@ public class CookieAuthenticationStateProvider(
         return _isAuthenticated;
     }
 
-    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
     {
-        throw new NotImplementedException();
+        _isAuthenticated = false;
+
+        var user = new ClaimsPrincipal(new ClaimsIdentity());
+
+        var userInfo = await GetUser();
+
+        if (userInfo is null)
+            return new AuthenticationState(user);
+
+        var claims = await GetClaims(userInfo);
+
+        var id = new ClaimsIdentity(claims, nameof(CookieAuthenticationStateProvider));
+
+        user = new ClaimsPrincipal(id);
+
+        _isAuthenticated = true;
+        return new AuthenticationState(user);
     }
 
     private async Task<User?> GetUser()
