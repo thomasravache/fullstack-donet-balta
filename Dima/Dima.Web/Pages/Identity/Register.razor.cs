8443922d3dd2558e06a35adf65f5cc71db5b9c63
travache <thomasravache31@gmail.com>
2024-10-09 05:18:26 -0300

    feat: 106. Register - Code Behind

--- Dima/Dima.Web/Pages/Identity/Register.razor.cs
+++ Dima/Dima.Web/Pages/Identity/Register.razor.cs
@@ -1,4 +1,5 @@
 using Dima.Core.Handlers;
+using Dima.Core.Requests.Account;
 using Microsoft.AspNetCore.Components.Authorization;
 
 namespace Dima.Web.Pages.Identity;
@@ -16,7 +17,49 @@ public partial class RegisterComponentBase : ComponentBase
     protected MudForm? MudFormRef { get; set; }
     #endregion
 
+    #region Props
+    protected bool IsBusy { get; set; } = false;
+    protected RegisterRequest InputModel { get; set; } = new();
+    #endregion
+
+    #region LifeCycle
+    protected override async Task OnInitializedAsync()
+    {
+        var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+        var user = authState.User;
+
+        if (user is { Identity.IsAuthenticated: true })
+            NavigationManager.NavigateTo(PageRoutes.Comecar);
+    }
+    #endregion
+
     #region Methods
     protected static EmailAddressAttribute GetEmailValidator() => new() { ErrorMessage = "E-mail Inválido" };
+
+    public async Task OnValidSubmitAsync()
+    {
+        IsBusy = true;
+
+        try
+        {
+            var result = await AccountHandler.RegisterAsync(InputModel);
+
+            if (result.IsSuccess)
+            {
+                Snackbar.Add("Registro efetuado com sucesso!", Severity.Success);
+                NavigationManager.NavigateTo(PageRoutes.Login);
+            }
+            else
+                Snackbar.Add(result.PrincipalMessage ?? "Erro ao registrar um novo usuário.", Severity.Error);
+        }
+        catch (Exception ex)
+        {
+            Snackbar.Add(ex.Message, Severity.Error);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
     #endregion
 }
